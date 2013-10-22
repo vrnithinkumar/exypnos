bfcb90f57639449726ec87d8eaec9f18689c4563
nachivpn <nachivpn@gmail.com>
2013-10-22 09:23:20 +0530

    added matrix support, mean & variance caluclation

--- train_set.py
+++ train_set.py
@@ -1,11 +1,23 @@
 import random
+import numpy as np
 x = object()
-age = [x] * 10
-i = 0
-for j in age:
-	age[i] = int(random.gauss(40,10))
-	if age[i] < 20:
-		age[i] = age[i] + 20	
-	print "%d" % age[i]
-	i=i+i
-
+max = 50
+age = []
+cigs = []
+fv = []
+for j in range(0,max):
+	rga = int(random.gauss(40,10))
+	rgc = int(random.gauss(10,2))
+	age.append(rga)
+	cigs.append(rgc)
+	if age[j] < 20:
+		age[j] = age[j] + 20
+	if cigs[j] < 0:
+		cigs[j]=0	
+	fv_x = np.matrix([age[j],cigs[j]])
+	fv.append(fv_x)
+#print fv
+m = np.mean(fv,0,int)
+print m
+v = np.var(fv,0,int)
+print v
